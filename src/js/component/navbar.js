022f0c7572394243cc8e6486bb998928cc33b99f
Santiago Montoya <santiago.mordz@gmail.com>
2024-12-25 16:57:56 +0000

    styles(dropdow-menu): menu items alignment correction for better user experience

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,14 @@ import React, { useEffect, useRef, useContext, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 import { Context } from "../store/appContext";
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 export const Navbar = () => {
 	const { store, actions } = useContext(Context)
 	const [hoverIndex, setHoverIndex] = useState(null);
 	const [isDropDownOpen, setIsDropDownOpen] = useState(false)
 	const dropdownRef = useRef(null);
-
-	const toggleDropdown = () => {
-		setIsDropDownOpen(prev => !prev)
-	}
 
 	const handleClickOutside = (event) => {
 		if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
@@ -31,43 +29,39 @@ export const Navbar = () => {
 			<div className="container-fluid" style={{ width: "80%" }}>
 				<img style={{ width: "100px" }} src="https://pngimg.com/uploads/star_wars_logo/star_wars_logo_PNG37.png" />
 
-				<div>
-					<div className="dropdown" ref={dropdownRef}>
-						<button
-							className="btn btn-primary dropdown-toggle"
-							type="button"
-							onClick={toggleDropdown}
-							aria-expanded="false">
-							Favorites
-						</button>
-						<ul className={`dropdown-menu ${isDropDownOpen ? 'show' : ''}`}>
-							{store.favorites.length === 0 ? (
-								<li className="dropdown-item">No favorites added!</li>
-							) : (
-								store.favorites.map((favorite, index) => (
-									<li
-										key={index}
-										className="dropdown-item d-flex justify-content-between"
-										onMouseEnter={() => setHoverIndex(index)}
-										onMouseLeave={() => setHoverIndex(null)}
-									>
-										{favorite.name}
-										{hoverIndex === index && (
-											<button
-												className="btn btn-sm"
-												onClick={(e) => {
-													e.stopPropagation()
-													actions.removeFavorite(favorite.id)
-												}}>
-												<FontAwesomeIcon icon={faTrashAlt} />
-											</button>
-										)}
+				<div ref={dropdownRef}>
+					<DropdownButton
+						align="end"
+						title="Favorites"
+						id="dropdown-menu-align-end"
+						variant="primary"
+					>
+						{store.favorites.length === 0 ? (
+							<Dropdown.Item>No favorites added!</Dropdown.Item>
+						) : (
+							store.favorites.map((favorite, index) => (
+								<Dropdown.Item
+									key={index}
+									className="dropdown-item d-flex justify-content-between"
+									onMouseEnter={() => setHoverIndex(index)}
+									onMouseLeave={() => setHoverIndex(null)}
+								>
+									{favorite.name}
+									{hoverIndex === index && (
+										<button
+											className="btn btn-sm"
+											onClick={(e) => {
+												e.stopPropagation()
+												actions.removeFavorite(favorite.id)
+											}}>
+											<FontAwesomeIcon icon={faTrashAlt} />
+										</button>
+									)}
 
-									</li>
-								))
-							)}
-						</ul>
-					</div>
+								</Dropdown.Item>
+							))
+						)}
+					</DropdownButton>
 				</div>
 			</div>
 		</nav>
